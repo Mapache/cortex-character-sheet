540c409c7d8df4bb38622f64f2e3be1821a90fc1
Mapache <me@mapache.org>
2025-10-07 23:17:41 -0700

    Rename menu to toolbar

--- cortex/modules/sheet.js
+++ cortex/modules/sheet.js
@@ -15,18 +15,18 @@ window.add_trait_group = add_trait_group
 window.add_trait = add_trait
 window.remove_item = remove_item
 
-import { set_global_highlight_color, layoutControlsHidden, emptyDescriptionsHidden, show_help } from "./menu.js"
-window.set_global_highlight_color = set_global_highlight_color
-window.layoutControlsHidden = layoutControlsHidden
-window.emptyDescriptionsHidden = emptyDescriptionsHidden
-window.show_help = show_help
-
 import { show_modal, close_modal } from "./modal.js"
 window.show_modal = show_modal
 window.close_modal = close_modal
 
 import { download_character } from "./save.js"
 window.download_character = download_character
+
+import { set_global_highlight_color, layoutControlsHidden, emptyDescriptionsHidden, show_help } from "./toolbar.js"
+window.set_global_highlight_color = set_global_highlight_color
+window.layoutControlsHidden = layoutControlsHidden
+window.emptyDescriptionsHidden = emptyDescriptionsHidden
+window.show_help = show_help
 
 import { show_context_menu, set_trait_collection_highlight_color, remove_trait_collection_highlight_color, set_style, context_menu_remove_item, move_to_top, move_to_bottom, move_up, move_down, move_to_other_column, move_to_next_page, move_to_previous_page } from "./traitGroupContextMenu.js"
 window.show_context_menu = show_context_menu
